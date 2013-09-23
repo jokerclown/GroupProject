492f7943f44f51264c05d720b01a32b0b468211e
jokerclown <izzannuralam@gmail.com>
2013-09-23 11:20:16 +1000

    Update mode 0

--- TSPi/src/Mode0.java
+++ TSPi/src/Mode0.java
@@ -6,7 +6,7 @@ import java.util.Random;
 
 
 public class Mode0 {
-	
+
 	public ArrayList<Character> generateSet(){
 
 		ArrayList<Character> newSet = new ArrayList<Character>(Arrays.asList(
@@ -18,9 +18,9 @@ public class Mode0 {
 
 		return newSet;
 	}
-	
+
 	public ArrayList<String> randomChar(ArrayList<Character> availableChar){
-		
+
 		ArrayList<String> result = new ArrayList<String>();
 		Random rand = new Random();
 		int randomNumber = rand.nextInt(availableChar.size()-1);
@@ -30,28 +30,28 @@ public class Mode0 {
 
 		return result;
 	}
-	
+
 	public boolean checkParentExclude(ArrayList<Character> excludeParent, char parent){
 		boolean result = false;
-		
+
 		for(int i=0; i<excludeParent.size(); i++){
 			if(excludeParent.get(i)==parent){
 				result = true;
 				return result;
 			}
 		}
-		
+
 		return result;
 	}
-	
+
 	public char generateCharParent(ArrayList<Character> availableChar, ArrayList<Character> excludeParent){
 		char result = Character.UNASSIGNED;
 		boolean generateParent = true;
-		
+
 		while(generateParent){
 
 			result = this.randomChar(availableChar).get(1).charAt(0);
-			
+
 			switch(excludeParent.size()){
 			case 0:
 				generateParent = false;
@@ -65,15 +65,87 @@ public class Mode0 {
 			}
 
 		}
+
+		return result;
+	}
+
+	public boolean checkSameChild(ArrayList<Character> childContainer, char child){
+		boolean result = false;
+
+		for(int m = 0; m<childContainer.size();m++){
+
+			if(childContainer.get(m)==child){
+				result = true;
+			}
+
+		}
+
+		return result;
+	}
+	
+	public int checkChildIsParent(ArrayList<ArrayList<Object>> resultContainer, char child){
+		int result = 0;
+		
+		for(int i = 0; i<resultContainer.size(); i++){
+			if(resultContainer.get(i).get(0).toString().charAt(0)==child){
+				result = i;
+			}
+		}
 		
 		return result;
 	}
-	
-	public char generateCharChild(){
-		char result = Character.UNASSIGNED;
+
+	public boolean checkChildParentDependent(ArrayList<ArrayList<Object>> resultContainer, char child, char parent){
+		boolean result = false;
+
+		int indexParent = this.checkChildIsParent(resultContainer, child);
+
+		if(indexParent!=0){
+
+			ArrayList<Character> focusChild = (ArrayList<Character>) resultContainer.get(indexParent).get(1);
+
+			for(int l = 0; l<focusChild.size(); l++){
+
+				if(focusChild.get(l)==parent){
+					result = true;
+				}
+
+			}
+		}
+
 		return result;
 	}
 	
+	public void removeParentFromChildAvailable(ArrayList<Character> availableChar, char parent){
+		for(int i=0; i<availableChar.size(); i++){
+			
+			if(availableChar.get(i)==parent){
+				availableChar.remove(i);
+			}
+			
+		}
+	}
+
+	public char generateCharChild(ArrayList<ArrayList<Object>> resultContainer, ArrayList<Character> childContainer, char child, char parent){
+		char result = Character.UNASSIGNED;
+		ArrayList<Character> availableChar = this.generateSet();
+		this.removeParentFromChildAvailable(availableChar, parent);
+
+		boolean generateChild = true;
+
+		while(generateChild){
+			
+			result = this.randomChar(availableChar).get(1).charAt(0);
+			
+			boolean sameChild = this.checkSameChild(childContainer, child);
+			boolean ChildParentDependent = this.checkChildParentDependent(resultContainer, child, parent);
+			
+			
+		}
+
+		return result;
+	}
+
 	public ArrayList<ArrayList<Object>> generateFPO(int lengthX, int lengthY){
 		ArrayList<ArrayList<Object>> result = new ArrayList<ArrayList<Object>>();
 		ArrayList<Character> availableChar = this.generateSet();
@@ -166,7 +238,7 @@ public class Mode0 {
 
 		return result;
 	}
-	
+
 	public void generateText(ArrayList<ArrayList<Object>> inputFPO){
 
 		try{
@@ -174,26 +246,26 @@ public class Mode0 {
 			FileWriter fstream = new FileWriter("textFile.txt");
 			BufferedWriter out = new BufferedWriter(fstream);
 			String oneLine = "";
-			
+
 			for(int i = 0; i<inputFPO.size(); i++){
-				
+
 				ArrayList<Object> focusLine = inputFPO.get(i);
-				
+
 				oneLine = focusLine.get(0).toString() + ":";
-				
+
 				ArrayList<Character> focusChild = (ArrayList<Character>) focusLine.get(1);
-				
+
 				for(int j =0; j<focusChild.size(); j++){
 					oneLine = oneLine + focusChild.get(j);
-					
+
 					if(j!=inputFPO.size()-1){
 						oneLine = oneLine + ",";
 					}
 				}
-				
+
 				out.write(oneLine);
 				out.newLine();
-				
+
 			}
 
 		}catch (Exception e){
@@ -204,9 +276,9 @@ public class Mode0 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
+
 		Mode0 test = new Mode0();
-		
+
 		ArrayList<ArrayList<Object>> tes = test.generateFPO(10, 5);
 		test.generateText(tes);
 		System.out.println(tes);
