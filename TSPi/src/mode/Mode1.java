01df193d7f908a618f5bd779f0a577b3da6e3bd0
jokerclown <izzannuralam@gmail.com>
2013-09-30 15:19:21 +1000

    edit mode1

--- TSPi/src/mode/Mode1.java
+++ TSPi/src/mode/Mode1.java
@@ -11,42 +11,6 @@ import java.util.ArrayList;
 
 public class Mode1 {
 
-	public ArrayList<ArrayList<Object>> readText(String fileName){
-		ArrayList<ArrayList<Object>> result = new ArrayList<ArrayList<Object>>();
-
-		try
-		{			
-			FileInputStream fileStream = new FileInputStream(fileName);
-			DataInputStream inStream = new DataInputStream(fileStream);
-			BufferedReader bufRead = new BufferedReader(new InputStreamReader(inStream));
-
-			String strLine;
-			while ((strLine = bufRead.readLine()) != null)   {
-				ArrayList<Character> childContainer = new ArrayList<Character>();
-				ArrayList<Object> lineContainer = new ArrayList<Object>();
-
-				String[] splitStr = strLine.split(":");
-				char parent = splitStr[0].charAt(0);
-
-				String[] splitChild = splitStr[1].split(",");
-
-				for(int i = 0; i < splitChild.length; i++){
-					char child = splitChild[i].charAt(0);
-					childContainer.add(child);
-				}
-				lineContainer.add(parent);
-				lineContainer.add(childContainer);
-				result.add(lineContainer);
-			}
-		}
-		catch(Exception e)
-		{
-			System.err.println("Error: " + e.getMessage());
-		}
-
-		return result;
-	}
-
 	public void addCount(ArrayList<ArrayList<Object>> count, char child){
 		if(count.size()==0){
 			ArrayList<Object> result = new ArrayList<Object>();
@@ -116,16 +80,6 @@ public class Mode1 {
 		}
 		
 		return result;
-	}
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		Mode1 test = new Mode1();
-		ArrayList<ArrayList<Object>> input = test.readText("textFile.txt");
-		System.out.println(input);
-		ArrayList<ArrayList<Object>> sov = test.generateSequenceOfValue(input);
-		System.out.println(sov);
-		System.out.println(test.sort(sov));
 	}
 
 }
